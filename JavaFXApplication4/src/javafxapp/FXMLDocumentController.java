9f2d934e2bf161adfcb0e3f5db44cccf10d9622f
Yerik1 <yerikchaves818@gmail.com>
2023-03-04 23:21:46 -0600

    1era version final

--- JavaFXApplication4/src/javafxapp/FXMLDocumentController.java
+++ JavaFXApplication4/src/javafxapp/FXMLDocumentController.java
@@ -5,6 +5,8 @@
 package javafxapp;
 
 import Tarea1Datos.Persona;
+import Tarea1Datos.Lista;
+import Tarea1Datos.Nodo;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -33,6 +35,7 @@ public class FXMLDocumentController implements Initializable {
     private Label lbResult;
     @FXML
     private ChoiceBox<String> cbProvincia;
+    Lista lista= new Lista(); 
 
     /**
      * Initializes the controller class.
@@ -45,25 +48,66 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void btnSuma(ActionEvent event) {
+        int x=lista.buscar(cbPers1.getValue()).getEdad();
+        int y=lista.buscar(cbPers2.getValue()).getEdad();
+        int result=x+y;
+        String res=Integer.toString(result);
+        lbResult.setText(res);
     }
 
     @FXML
     private void btnAgregar(ActionEvent event) {
-        Persona persona = new Persona(tfNombre.getText(),Integer.parseInt(tfEdad.getText()),cbProvincia.getValue());
-        cbPers1.getItems().add(persona.getNombre());
-        cbPers2.getItems().add(persona.getNombre());
+        lbResult.setText("");
+        if(lista.getLargo()<=3){
+            try{
+                if(cbProvincia.getValue()!=null){
+                    if(tfNombre.getText().length()!=0){
+                        Persona persona = new Persona(tfNombre.getText(),Integer.parseInt(tfEdad.getText()),cbProvincia.getValue());
+                        cbPers1.getItems().add(persona.getNombre());
+                        cbPers2.getItems().add(persona.getNombre());
+                        lista.agregar(new Nodo(persona));
+                    }else{
+                        lbResult.setText("Por favor ingrese un nombre");
+                    }
+                }else{
+                    lbResult.setText("Por favor ingrese una provincia");
+                }
+            }catch(NumberFormatException e){
+                lbResult.setText("Por favor ingrese un numero como edad");
+            }
+        }else{
+            lbResult.setText("Solo se permiten 4 personas");
+        }
+        tfNombre.setText("");
+        tfEdad.setText("");
+        
     }
 
     @FXML
     private void btnResta(ActionEvent event) {
+        int x=lista.buscar(cbPers1.getValue()).getEdad();
+        int y=lista.buscar(cbPers2.getValue()).getEdad();
+        int result=x-y;
+        String res=Integer.toString(result);
+        lbResult.setText(res);
     }
 
     @FXML
     private void btnMult(ActionEvent event) {
+        int x=lista.buscar(cbPers1.getValue()).getEdad();
+        int y=lista.buscar(cbPers2.getValue()).getEdad();
+        int result=x*y;
+        String res=Integer.toString(result);
+        lbResult.setText(res);
     }
 
     @FXML
     private void btnDiv(ActionEvent event) {
+        float x=lista.buscar(cbPers1.getValue()).getEdad();
+        float y=lista.buscar(cbPers2.getValue()).getEdad();
+        double result=(x/y);
+        String res=Double.toString(result);
+        lbResult.setText(res);
     }
     
 }
